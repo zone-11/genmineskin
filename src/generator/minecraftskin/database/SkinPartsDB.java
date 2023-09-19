295bb031189c1e3a6b3daf43b1c521c713a217cf
flytomoon <ivann1812@gmail.com>
2023-09-19 19:33:57 +0300

    Added some methods: exists(), saveSkinPart(), saveSkinParts(). Also a little refactoring. DEVELOP

--- src/generator/minecraftskin/database/SkinPartsDB.java
+++ src/generator/minecraftskin/database/SkinPartsDB.java
@@ -1,7 +1,16 @@
 package generator.minecraftskin.database;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
+import generator.minecraftskin.exceptions.UnhandledSkinSizeException;
+import generator.minecraftskin.skinpart.MinecraftSkinPart;
+import generator.minecraftskin.skinpart.SkinPart;
 
 public class SkinPartsDB {
 	
@@ -10,31 +19,29 @@ public class SkinPartsDB {
 	
 	private final File fileDB;
 	
-	private File headsPathDB;
-	private File bodiesPathDB;
-	private File handsPathDB;
-	private File legsPathDB;
+	private final File headsFileDB;
+	private final File bodiesFileDB;
+	private final File handsFileDB;
+	private final File legsFileDB;
 	
 	public SkinPartsDB(String nameOfNewDB) {
-		
 		if (!nameOfNewDB.matches(pattern.pattern())) {
 			throw new IllegalArgumentException("wrong name of db");
 		}
 		
 		this.fileDB = new File(SKIN_PARTS_DB, nameOfNewDB);
 		
+		this.headsFileDB = new File(fileDB, "heads");
+		this.bodiesFileDB = new File(fileDB, "bodies");
+		this.handsFileDB = new File(fileDB, "hands");
+		this.legsFileDB = new File(fileDB, "legs");
 	}
 	
 	public void makeDB() {
-		File[] files = { headsPathDB, bodiesPathDB, handsPathDB, legsPathDB };
-		String[] dirs = { "heads", "bodies", "hands", "legs" };
+		var files = Arrays.asList(headsFileDB, bodiesFileDB, handsFileDB,
+								legsFileDB);
 		
-		for (int i = 0; i < files.length ; i++) {
-			
-			files[i] = new File(fileDB, dirs[i]);
-			
-			files[i].mkdirs();
-		}
+		files.forEach(File::mkdirs);
 	}
 	
 	public void delete() {
@@ -48,13 +55,55 @@ public class SkinPartsDB {
 		
 		fileDB.delete();
 	}
+	
+	public boolean exists() {
+		return fileDB.exists();
+	}
+	
+	public void saveSkinParts(List<MinecraftSkinPart> parts) {
+		parts.forEach(this::saveSkinPart);
+	}
+	
+	public void saveSkinPart(MinecraftSkinPart skinPart) {
+		try {
+			var fileToSave = new File(
+				switch (skinPart.getSkinPartType()) {
+				case HEAD -> headsFileDB.getAbsolutePath() + "\\"
+				+ headsFileDB.listFiles().length;
+				case BODY -> bodiesFileDB.getAbsolutePath() + "\\" 
+				+ bodiesFileDB.listFiles().length;
+				case HAND1 -> handsFileDB.getAbsolutePath() + "\\" 
+				+ handsFileDB.listFiles().length;
+				case HAND2 -> handsFileDB.getAbsolutePath() + "\\" 
+				+ handsFileDB.listFiles().length;
+				case LEG1 -> legsFileDB.getAbsolutePath() + "\\" 
+				+ legsFileDB.listFiles().length;
+				case LEG2 -> legsFileDB.getAbsolutePath() + "\\" 
+				+ legsFileDB.listFiles().length;
+				
+				default -> throw new IllegalArgumentException("uncorrect MinecraftSkinPart type");
+				}
+				+ ".png"
+			);
+			
+			fileToSave.createNewFile();
+			ImageIO.write(skinPart.getImage(), "png", fileToSave);
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 
-	public static void main(String...args) {
+	public static void main(String...args) throws UnhandledSkinSizeException, IOException {
 		SkinPartsDB skinPartsDB = new SkinPartsDB("superDataBase");
+		var file = new File("C:\\projects\\test_package\\deadpool.png");
+		var skinPart = MinecraftSkinPart.createSkinPart(ImageIO.read(file), SkinPart.HEAD);
 		
-		skinPartsDB.makeDB();
 		skinPartsDB.delete();
+		skinPartsDB.makeDB();
+		skinPartsDB.saveSkinPart(skinPart);
+		skinPartsDB.saveSkinParts(Arrays.asList(skinPart, skinPart, skinPart));
 	}
 			
 }
